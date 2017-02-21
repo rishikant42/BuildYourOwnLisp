b31b17bb593ce25008efdf014c95878d4336b328
rishikant42 <rksbtp@gmail.com>
2017-02-21 19:13:50 +0530

    fixed version

--- blispy.c
+++ blispy.c
@@ -141,7 +141,7 @@ int main(int argc, char** argv) {
       ",
       Number, Operator, Expr, Lispy);
 
-  puts("Lispy Version 0.0.0.0.4");
+  puts("Lispy Version 0.1");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
